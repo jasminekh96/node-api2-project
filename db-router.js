41e0e01aaf216981c5be407658d81028e8374427
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-12-03 18:03:43 -0500

    posts and delete finished

--- db-router.js
+++ db-router.js
@@ -37,7 +37,7 @@ router.get('/:id', (req, res) => {
 		});
 });
 router.get('/:id/comments', (req, res) => {
-	DataBase.findCommentById(req.params.id)
+	DataBase.findPostComments(req.params.id)
 		.then((posts) => {
 			if (posts) {
 				res.status(200).json(posts);
@@ -71,6 +71,44 @@ router.post('/', (req, res) => {
 				});
 			});
 	}
+});
+
+router.post('/:id/comments', (req, res) => {
+	const comment = req.body;
+	if (!comment.text) {
+		res.status(400).json({ errorMessage: 'Please provide text for the comment.' });
+	} else {
+		comment.post_id = req.params.id;
+		DataBase.insertComment(comment)
+			.then((id) => {
+				DataBase.findPostComments(id).then((posts) => {
+					res.status(201).json(posts);
+				});
+			})
+			.catch((error) => {
+				console.log('error on the POST for posting a new text', error);
+				res.status(500).json({
+					errorMessage : 'There was an error while saving the comment to the database',
+				});
+			});
+	}
+});
+
+router.delete('/:id', (req, res) => {
+	DataBase.remove(req.params.id)
+		.then((count) => {
+			if (count > 0) {
+				res.status(200).json({ message: 'The hub has been removed' });
+			} else {
+				res.status(400).json({ errorMessage: 'The post with the specified ID does not exist.' });
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({
+				message : 'The post could not be removed',
+			});
+		});
 });
 
 module.exports = router;
